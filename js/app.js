8905262e624bb64523cf11cc2f658f61c55e84f8
salihoosainionr <salihoosainione@gmail.com>
2018-09-16 18:56:31 +0430

    Fazelab form get inputs value

--- js/app.js
+++ js/app.js
@@ -1,70 +1,70 @@
 $(document).foundation();
 $("#selectForm_tabs").owlCarousel({
-	rtl			: true,
-	loop		: false,
-	margin		: 20,
-	nav			: false,
-	dots 		: false,
-	items 		: 3,
+    rtl: true,
+    loop: false,
+    margin: 20,
+    nav: false,
+    dots: false,
+    items: 3,
 });
 $('.categories_list').owlCarousel({
-	rtl			: true,
-	loop		: true,
-	margin		: 20,
-	nav			: true,
-	dots 		: false,
-	responsiveClass: true,
-	responsive: {
-		0: {
-			items: 2,
-			margin: 10
-		},
-		350: {
-			items: 3,
-			margin: 10
-		},
-		500: {
-			items: 4
-		},
-		800: {
-			items: 5
-		},
-		1000: {
-			items: 6
-		}
-	}
+    rtl: true,
+    loop: true,
+    margin: 20,
+    nav: true,
+    dots: false,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 2,
+            margin: 10
+        },
+        350: {
+            items: 3,
+            margin: 10
+        },
+        500: {
+            items: 4
+        },
+        800: {
+            items: 5
+        },
+        1000: {
+            items: 6
+        }
+    }
 });
 $('.news_slider').owlCarousel({
-	rtl			: true,
-	loop		: true,
-	margin		: 20,
-	nav			: true,
-	dots 		: false,
-	responsiveClass: true,
-	responsive: {
-		0: {
-			items: 1
-		},
-		350: {
-			items: 1
-		},
-		500: {
-			items: 2
-		},
-		800: {
-			items: 2
-		},
-		1000: {
-			items: 2
-		}
-	}
+    rtl: true,
+    loop: true,
+    margin: 20,
+    nav: true,
+    dots: false,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        350: {
+            items: 1
+        },
+        500: {
+            items: 2
+        },
+        800: {
+            items: 2
+        },
+        1000: {
+            items: 2
+        }
+    }
 });
 $('.customer_reviews_slider').owlCarousel({
-	rtl			: true,
-	items 		: 1,
-	loop		: true,
-	nav			: true,
-	dots 		: false
+    rtl: true,
+    items: 1,
+    loop: true,
+    nav: true,
+    dots: false
 });
 
 // Fazelab Script Section
@@ -119,6 +119,7 @@ for (i = 0; i < x.length; i++) {
         this.classList.toggle("select-arrow-active");
     });
 }
+
 function closeAllSelect(elmnt) {
     /*a function that will close all select boxes in the document,
     except the current select box:*/
@@ -138,6 +139,7 @@ function closeAllSelect(elmnt) {
         }
     }
 }
+
 /*if the user clicks anywhere outside the select box,
 then close all select boxes:*/
 document.addEventListener("click", closeAllSelect);
@@ -145,9 +147,9 @@ document.addEventListener("click", closeAllSelect);
 
 
 /**
-   * Ripple effect mechanism
-   */
-  $('body').on('click', '.ripple-effect', function(e) {
+ * Ripple effect mechanism
+ */
+$('body').on('click', '.ripple-effect', function (e) {
     // Ignore default behavior
     e.preventDefault();
 
@@ -160,20 +162,20 @@ document.addEventListener("click", closeAllSelect);
     // Get custom color for ripple effect
     var color = the_dom.attr('data-ripple-color');
     if (typeof color == 'undefined') {
-      var color = 'rgba( 255, 255, 255, 0.1 )';
+        var color = 'rgba( 255, 255, 255, 0.1 )';
     }
 
     // Get ripple radius
     var radius = the_dom.attr('data-ripple-wrap-radius');
     if (typeof radius == 'undefined') {
-      var radius = 0;
+        var radius = 0;
     }
 
     // Get the clicked element and the click positions
     if (typeof limit == 'undefined') {
-      var the_dom_limit = the_dom;
+        var the_dom_limit = the_dom;
     } else {
-      var the_dom_limit = the_dom.closest(limit);
+        var the_dom_limit = the_dom.closest(limit);
     }
 
     var the_dom_offset = the_dom_limit.offset();
@@ -187,23 +189,23 @@ document.addEventListener("click", closeAllSelect);
     // Draw the ripple effect wrap
     var ripple_effect_wrap = $('<span class="ripple-effect-wrap"></span>');
     ripple_effect_wrap.css({
-      'width': the_dom_width,
-      'height': the_dom_height,
-      'position': 'absolute',
-      'top': the_dom_offset.top,
-      'left': the_dom_offset.left,
-      'z-index': 10000,
-      'overflow': 'hidden',
-      'background-clip': 'padding-box',
-      '-webkit-border-radius': radius,
-      'border-radius': radius
+        'width': the_dom_width,
+        'height': the_dom_height,
+        'position': 'absolute',
+        'top': the_dom_offset.top,
+        'left': the_dom_offset.left,
+        'z-index': 10000,
+        'overflow': 'hidden',
+        'background-clip': 'padding-box',
+        '-webkit-border-radius': radius,
+        'border-radius': radius
     });
 
     // Adding custom class, it is sometimes needed for customization
     var ripple_effect_wrap_class = the_dom.attr('data-ripple-wrap-class');
 
     if (typeof ripple_effect_wrap_class != 'undefined') {
-      ripple_effect_wrap.addClass(ripple_effect_wrap_class);
+        ripple_effect_wrap.addClass(ripple_effect_wrap_class);
     }
 
     // Append the ripple effect wrap
@@ -217,30 +219,30 @@ document.addEventListener("click", closeAllSelect);
     // Draw the ripple effect
     var ripple = $('<span class="ripple"></span>');
     ripple.css({
-      'width': circular_width,
-      'height': circular_width,
-      'background': color,
-      'position': 'absolute',
-      'top': click_y_ripple - (circular_width / 2),
-      'left': click_x_ripple - (circular_width / 2),
-      'content': '',
-      'background-clip': 'padding-box',
-      '-webkit-border-radius': '50%',
-      'border-radius': '50%',
-      '-webkit-animation-name': 'ripple-animation',
-      'animation-name': 'ripple-animation',
-      '-webkit-animation-duration': '2s',
-      'animation-duration': '2s',
-      '-webkit-animation-fill-mode': 'both',
-      'animation-fill-mode': 'both'
+        'width': circular_width,
+        'height': circular_width,
+        'background': color,
+        'position': 'absolute',
+        'top': click_y_ripple - (circular_width / 2),
+        'left': click_x_ripple - (circular_width / 2),
+        'content': '',
+        'background-clip': 'padding-box',
+        '-webkit-border-radius': '50%',
+        'border-radius': '50%',
+        '-webkit-animation-name': 'ripple-animation',
+        'animation-name': 'ripple-animation',
+        '-webkit-animation-duration': '2s',
+        'animation-duration': '2s',
+        '-webkit-animation-fill-mode': 'both',
+        'animation-fill-mode': 'both'
     });
     $('.ripple-effect-wrap:last').append(ripple);
 
     // Remove rippling component after half second
-    setTimeout(function() {
-      ripple_effect_wrap.fadeOut(function() {
-        $(this).remove();
-      });
+    setTimeout(function () {
+        ripple_effect_wrap.fadeOut(function () {
+            $(this).remove();
+        });
     }, 500);
 
     // Get the href
@@ -248,27 +250,27 @@ document.addEventListener("click", closeAllSelect);
     var href = the_dom.attr('href');
     var target = the_dom.attr('target');
     if (!target) {
-      target = '_self';
+        target = '_self';
     }
     // Safari appears to ignore all the effect if the clicked link is not prevented using preventDefault()
     // To overcome this, preventDefault any clicked link
     // If this isn't hash, redirect to the given link
     if (typeof href != 'undefined' && href.substring(0, 1) != '#') {
-      setTimeout(function() {
-        window.open(href, target);
-      }, 200);
+        setTimeout(function () {
+            window.open(href, target);
+        }, 200);
     }
 
     // Ugly manual hack to fix input issue
     if (the_dom.is('input') || the_dom.is('button')) {
-      setTimeout(function() {
-        the_dom.removeClass('ripple-effect');
-        the_dom.trigger('click');
-        the_dom.addClass('ripple-effect');
-      }, 200);
+        setTimeout(function () {
+            the_dom.removeClass('ripple-effect');
+            the_dom.trigger('click');
+            the_dom.addClass('ripple-effect');
+        }, 200);
     }
 
-  });
+});
 //   ====== end ripple effect
 
 
